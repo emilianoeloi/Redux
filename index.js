81c02e733042d891d9490ba71b24e13ee95a1901
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2016-02-19 10:38:46 -0200

    ADD: Writing a counter function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function counter(state, action){
+const counter = (state = 0, action) => {
+  
+  if (typeof state === 'undefined'){
+    return 0;
+  }
+  
   if(action.type === 'INCREMENT'){
     return state + 1;
   }else if(action.type === 'DECREMENT'){
     return state - 1;
+  }else{
+    return state;
   }
 }
 
@@ -20,6 +27,14 @@ expect(
 
 expect(
   counter(1, {type: 'DECREMENT'})
+).toEqual(0);
+
+expect(
+  counter(1, {type:'ANYTHING_ELSE'})
+).toEqual(1);
+
+expect(
+  counter(undefined, {})
 ).toEqual(0);
 
 console.info('Test Passed!');
